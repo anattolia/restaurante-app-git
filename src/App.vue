8b0c162162584da106034f8d0301498983cd4510
Antonia Bustamante <anattolia@gmail.com>
2020-12-11 19:56:23 -0500

    cliente

--- src/App.vue
+++ src/App.vue
@@ -1,108 +1,53 @@
 <template>
-  <div id="app">
-    <div class="header">
-      <h1>Banco UN</h1>
-      <nav>
-        <button v-on:click="init" v-if="is_auth">Inicio</button>
-        <button v-on:click="getBalance" v-if="is_auth">Saldo</button>
-        <button v-if="is_auth">Transacción</button>
-        <button v-if="is_auth">Cerrar Sesión</button>
-      </nav>
-    </div>
-
-    <div class="main-component">
-      <router-view></router-view>
-    </div>
-
-    <div class="footer">
-      <h2>Misión TIC 2022</h2>
-    </div>
+  <div id="UserBalance">
+    <h2>{{ username }}</h2>
+    <h2>
+      Tu saldo es: <span> {{ balance }} COP </span>
+    </h2>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: "App",
-
-  components: {},
-
-  data: function () {
+  name: "UserBalance",
+  data: function() {
     return {
-      is_auth: localStorage.getItem("isAuth") || false,
+      username: "",
+      balance: 0
     };
   },
-  methods: {
-    init: function(){
-      if(this.$route.name != "user") {
-        let username = localStorage.getItem("current_username");
-        this.$router.push({ name: "user", params: {username:username} });
-    }
-  },
-  getBalance: function(){
-    if(this.$route.name != "user_balance"){
-      let username = localStorage.getItem("current_username")
-      this.$router.push({ name:"user_balance", params:{username:username} });
-      }
-  },
-
-},
-beforeCreate: function(){
-  localStorage.setItem("current_username", "camilo24");
-  localStorage.setItem("isAuth", true);
-
-  this.$router.push({name: "user", params: { username: "camilo24"} });
+  created: function() {
+    this.username = this.$route.params.username;
+    let self = this;
+    axios
+      .get("http://127.0.0.1:8000/user/balance/" + this.username)
+      .then(result => {
+        self.balance = result.data.balance;
+      })
+      .catch(error => {
+        alert("ERROR Servidor");
+      });
   }
 };
 </script>
 
 <style>
-body {
-  margin: 0 0 0 0;
-}
-.header {
-  margin: 0%;
-  padding: 0;
-  width: 100%;
-  height: 10vh;
-  min-height: 100px;
-  background-color: #283747;
-  color: #e5e7e9;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-.header nav button {
-  color: #e5e7e9;
-  background: #283747;
-  border: 1px solid #e5e7e9;
-  border-radius: 5px;
-  padding: 10px 20px;
-}
-.header nav button:hover {
-  color: #283747;
-  background: #e5e7e9;
-  border: 1px solid #e5e7e9;
-}
-.main-component {
-  height: 75vh;
-  margin: 0%;
-  padding: 0%;
-  background: #fdfefe;
-}
-.footer {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  height: 10vh;
-  min-height: 100px;
-  background-color: #283747;
-  color: #e5e7e9;
-}
-.footer h2 {
+#UserBalance {
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+
+#UserBalance h2 {
+  font-size: 50px;
+  color: #283747;
+}
+#UserBalance span {
+  color: crimson;
+  font-weight: bold;
 }
 </style>
---
+++ src/components/Cliente.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="Cliente">
+    <!-- El formulario para acceder: -->
+    <form id="form1" onsubmit="getFormvalue()">
+        "Nombre y apellido: "<br>
+        <input type="text" name="nombre" value><br><br>
+         "Teléfono: "<br>
+        <input type="text" name="telefono" value><br><br>
+        "Cédula: "<br>
+        <input type="text" name="cedula" value><br><br>
+        "Dirección: "<br>
+        <input type="text" name="direccion" value><br><br>
+        "Barrio: "<br>
+        <input type="text" name="barrio" value><br><br>
+        <input type="submit" value="Crear">
+        <input type="submit" value="Buscar">
+        <input type="submit" value="Modificar">
+        <input type="submit" value="Eliminar">
+      </form>
+    <h2>
+      Hola <span> {{ clientename }}, </span> ¡Bienvenido!
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cliente",
+  data: function() {
+    return {
+      clientename: "none"
+    };
+  },
+  created: function() {
+    this.clientename = this.$route.params.clientename;
+  }
+};
+</script>
+
+<style>
+#User {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #06080c80;
+}
+#User h2 {
+  font-size: 50px;
+  color: #d6d6d6;
+}
+#User span {
+  color: crimson;
+  font-weight: bold;
+}
+</style>
